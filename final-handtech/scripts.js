d378135fbfb74e6a1845909eb52d951b34186ec8
Elizabeth <elizabethrichards@Elizabeths-MBP.wireless.newschool.edu>
2018-05-03 18:42:28 -0400

    files

---
+++ final-handtech/scripts.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // function play(){
+ //       var audio = document.getElementById("beat1");
+ //       audio.play();
+ //                 }
+ //      function play2(){
+ //       var audio = document.getElementById("beat2");
+ //       audio.play();
+ //                 }
+
+ //   function play3(){
+ //   var audio = document.getElementById("beat3");
+ //    audio.play();
+ //                 }
+
+ //   function play4(){
+ //       var audio = document.getElementById("beat4");
+ //       audio.play();
+ //                 }
+
+
+
